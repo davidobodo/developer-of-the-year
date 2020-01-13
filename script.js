89074c09af93e5b6b88422a361e73899d5e4fe2f
David Obodo <obododavid5@gmail.com>
2020-01-13 13:17:54 +0100

    include rotation of image into hover effect

--- script.js
+++ script.js
@@ -27,14 +27,15 @@ displayPins();
 const CLIENT_WIDTH = document.documentElement.clientWidth;
 const CLIENT_HEIGHT = document.documentElement.clientHeight;
 const DISPLACEMENT_VALUES = [50, 70, 50, 30,];
+const ROTATION_ANGLE = [12, -12, -15, 15]
 
 
 const elements = document.querySelectorAll('[data-depth]');
 
 const handleMouseMove = (event) => {
     elements.forEach(o => {
-        const { x, y } = computeDisplacement(event, o);
-        updateStyle(o, x, y);
+        const { x, y, z } = computeDisplacement(event, o);
+        updateStyle(o, x, y, z);
     })
 };
 
@@ -43,15 +44,16 @@ const computeDisplacement = (event, element) => {
 
     const displacementDepth = element.dataset.depth;
     const displacementValue = DISPLACEMENT_VALUES[Number(displacementDepth) - 1];
+    const z = ROTATION_ANGLE[Number(displacementDepth) - 1];
 
     const x = mouseX * displacementValue / CLIENT_WIDTH;
     const y = mouseY * displacementValue / CLIENT_HEIGHT;
 
-    return { x, y };
+    return { x, y, z };
 };
 
-const updateStyle = (element, x, y) => {
-    element.style.transform = `translate3d(${-x}px, ${-y}px, 0)`
+const updateStyle = (element, x, y, z) => {
+    element.style.transform = `translate3d(${-x}px, ${-y}px, 0) rotate(${z}deg)`
 }
 
 document.addEventListener('mousemove', handleMouseMove);
